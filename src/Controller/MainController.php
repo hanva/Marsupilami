a0cefb15eac67a603108338fae65beca1178ab7f
Nicolas <nicolas.tran@supinternet.fr>
2018-05-07 15:59:39 +0200

    add register

---
+++ src/Controller/MainController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 07/05/2018
+ * Time: 15:28
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/register")
+     */
+    public function register()
+    {
+        return $this->render('register.html.twig');
+    }
+}
